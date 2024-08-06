d705f8aad8d4a74b9cbc9d408f43163c45748239
amiyakiran <amiyakiran@gmail.com>
2024-08-06 13:36:43 +0530

    link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar className="bg-dark p-3">
       <Container>
         <Navbar.Brand>
-          <h4 className='text-warning'>
-            <FontAwesomeIcon icon={faVideo} beat className='me-3' />
-            media Player
-          </h4>
+          <Link to={'/'}>
+            <h4 className='text-warning'>
+              <FontAwesomeIcon icon={faVideo} beat className='me-3' />
+              media Player
+            </h4>
+          </Link>
         </Navbar.Brand>
       </Container>
     </Navbar>
